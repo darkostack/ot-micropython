d41b133b07f608b263ee114c6a9f233596daea0f
darkopancev <darkopancev.dev@gmail.com>
2019-02-12 17:26:34 +0800

    update third party micropython

--- examples/ping/main_2.py
+++ examples/ping/main_2.py
@@ -2,10 +2,12 @@ import ot
 
 thread = ot.Instance(2)
 
+thread.set_panid(0x1234)
+thread.set_channel(11)
+
 def main():
     if thread.is_initialized():
         thread.cli_uart_init()
-        thread.autocommission(0x1234, 11) #panid: 0x1234, channel: 11
         thread.autostart()
         while True:
             thread.tasklet_run()
